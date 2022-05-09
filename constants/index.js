24d198e6dab254157e00f91ef450a2a1a26ae370
Samly Panton <smov.org@gmail.com>
2022-05-09 05:37:30 +0000

    initial CD commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xc04EF05f9E85f6850088D99f3Ea7a5a5616C6402";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
