04d480ba1046b007c92a71fd946bf75a43ccd0bb
Samly Panton <smov.org@gmail.com>
2022-09-02 00:38:17 +0000

    Update

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -116,4 +116,24 @@
 
       // Fallback function is called when msg.data is not empty
       fallback() external payable {}
+
+      /**
+ * Minimal interface for CryptoDevsNFT containing only two functions
+ * that we are interested in
+ */
+interface ICryptoDevsNFT {
+    /// @dev balanceOf returns the number of NFTs owned by the given address
+    /// @param owner - address to fetch number of NFTs for
+    /// @return Returns the number of NFTs owned
+    function balanceOf(address owner) external view returns (uint256);
+
+    /// @dev tokenOfOwnerByIndex returns a tokenID at given index for owner
+    /// @param owner - address to fetch the NFT TokenID for
+    /// @param index - index of NFT in owned tokens array to fetch
+    /// @return Returns the TokenID of the NFT
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256);
+}
   }
